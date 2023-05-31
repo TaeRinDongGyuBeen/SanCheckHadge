c391f616cad5269e89408f2f8198995e39f19a32
dhk767 <dhk767@gmail.com>
2023-05-31 19:57:38 +0900

    [add] WalkingMapView 파일 생성

---
+++ InToTheSeoul/InToTheSeoul/View/WalkingMapView.swift
@@ -0,0 +1,26 @@
+//
+//  WalkingMapView.swift
+//  InToTheSeoul
+//
+//  Created by 김동현 on 2023/05/31.
+//
+
+import SwiftUI
+
+struct WalkingMapView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct WalkingMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        WalkingMapView()
+    }
+}
