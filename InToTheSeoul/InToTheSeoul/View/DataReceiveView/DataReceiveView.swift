b42b71ab5e231a0735ac63aaeb1e3ec5e7b90ce0
dhk767 <dhk767@gmail.com>
2023-06-04 02:39:28 +0900

    [ADD] DataReceiveView 생성

---
+++ InToTheSeoul/InToTheSeoul/View/DataReceiveView/DataReceiveView.swift
@@ -0,0 +1,35 @@
+//
+//  DataReceiveView.swift
+//  InToTheSeoul
+//
+//  Created by 김동현 on 2023/06/04.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("Hello World")
+            .textFontAndColor(.h7)
+    }
+}
+
+struct SampleView_Previewer: PreviewProvider {
+    static var previews: some View {
+        SampleView()
+    }
+}
+
+
+//struct SampleView: View {
+//    var body: some View {
+//        Text("Hello World")
+//            .textFontAndColor(.h7)
+//    }
+//}
+//
+//struct SampleView_Previewer: PreviewProvider {
+//    static var previews: some View {
+//        SampleView()
+//    }
+//}
