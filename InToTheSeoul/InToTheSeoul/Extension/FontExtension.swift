1a51543af073cd04ba6c56153bfdf05b0cf8dda7
dhk767 <dhk767@gmail.com>
2023-06-03 15:38:50 +0900

    [ADD] FontExtension enum 추가 중

---
+++ InToTheSeoul/InToTheSeoul/Extension/FontExtension.swift
@@ -0,0 +1,43 @@
+//
+//  FontExtension.swift
+//  InToTheSeoul
+//
+//  Created by 김동현 on 2023/06/03.
+//
+
+import SwiftUI
+
+extension Font {
+    enum Poppins {
+        case headline1
+        case headline2
+        case headline3
+        case headline4
+        case headline5
+        case headline6
+        case body1
+        case body2
+        case body3
+        case body4
+        case body5
+        case body6
+        case body7
+        case button1
+        case button2
+        
+//        var value: String {
+//            switch self {
+//            case .headline1:
+//                return "Poppins-SemiBold"
+//            case .headline2:
+//                return "Poppins-Medium"
+//            case .headline3:
+//                return
+//            }
+//        }
+    }
+
+//    static func poppins(_ type: Poppins, size: CGFloat = 17) -> Font {
+//        return .custom(type.value, size: size)
+//    }
+}
