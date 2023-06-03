fcc378c8278010d400a8102f72a35c503d0c0827
dhk767 <dhk767@gmail.com>
2023-06-03 22:02:18 +0900

    [DEL] ColorExtension hexcode 변환 init 삭제

--- InToTheSeoul/InToTheSeoul/Extension/ColorExtension.swift
+++ InToTheSeoul/InToTheSeoul/Extension/ColorExtension.swift
@@ -16,27 +16,6 @@ extension Color {
     ///     .foregroundColor(Color.theme.red)
     /// ```
     static let theme = ColorTheme()
-    
-    /// Hex 코드로 색 지정할 수 있도록 하는 생성자
-    /// ```
-    /// Color(hex: "EBEBEB")
-    /// ```
-    ///
-    /// - Parameters: String 형태의 Hex Code
-    ///
-    /// - returns: Hex 코드에 맞는 Color
-    init(hex: String) {
-      let scanner = Scanner(string: hex)
-      _ = scanner.scanString("#")
-      
-      var rgb: UInt64 = 0
-      scanner.scanHexInt64(&rgb)
-      
-      let r = Double((rgb >> 16) & 0xFF) / 255.0
-      let g = Double((rgb >>  8) & 0xFF) / 255.0
-      let b = Double((rgb >>  0) & 0xFF) / 255.0
-      self.init(red: r, green: g, blue: b)
-    }
 }
 
 struct ColorTheme {
