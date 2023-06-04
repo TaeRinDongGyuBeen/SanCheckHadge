d2afda278779c621f5aedb5db92ffcfd00bda89e
dhk767 <dhk767@gmail.com>
2023-06-04 21:52:42 +0900

    [ADD] MainView 파일 추가

---
+++ InToTheSeoul/InToTheSeoul/View/MainView/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  InToTheSeoul
+//
+//  Created by 김동현 on 2023/06/04.
+//
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello World")
+            .textFontAndColor(.h7)
+    }
+}
+
+struct MainView_Previewer: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
