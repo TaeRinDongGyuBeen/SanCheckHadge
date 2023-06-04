3810187dc11a3d7e6caa6f501fda2f2d49816c65
Taerogrammer <hyung54321@naver.com>
2023-06-04 22:55:28 +0900

    [Feat]SST#18 - 모달디자인 완료

--- InToTheSeoul/InToTheSeoul/View/TrackingView/TrackingModalView.swift
+++ InToTheSeoul/InToTheSeoul/View/TrackingView/TrackingModalView.swift
@@ -7,19 +7,70 @@
 
 import SwiftUI
 
-//struct TrackingModalView: View {
-//    var body: some View {
-//        VStack {
-//            Text("Hello")
-//        }
-//        .frame(maxWidth: .infinity)
-//        .frame(height: 100)
-//        .background(Color.red)
-//    }
-//}
-//
-//struct TrackingModalView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TrackingModalView()
-//    }
-//}
+struct TrackingModalView: View {
+    
+    @State var height: CGFloat = 100
+    let minHeight: CGFloat = 100
+    let maxHeight: CGFloat = 400
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            
+            //Handle
+            ZStack {
+                Capsule()
+                    .foregroundColor(Color.theme.gray3)
+                    .frame(width: 180, height: 10)
+            }
+            .frame(height: 40)
+            .frame(maxWidth: .infinity)
+            .gesture(dragGesture)
+            
+            VStack {
+                Text("Hello World")
+
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(.white)
+            )
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: height, alignment: .top)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture(minimumDistance: 0)
+            .onChanged { val in
+                
+                var newHeight = height - val.translation.height
+                
+                if newHeight > maxHeight {
+                    newHeight = maxHeight
+                }
+                else if newHeight < minHeight {
+                    newHeight = minHeight
+                }
+                
+                height = newHeight
+                
+                print("Drage : \(height)")
+                
+            }
+            .onEnded { val in
+                let percentage = height / maxHeight
+                var finalHeight = maxHeight
+                
+                if percentage < 0.45 {
+                    finalHeight = minHeight
+                }
+                withAnimation(Animation.easeOut(duration: 0.3)) {
+                    height = finalHeight
+                }
+            }
+    }
+}
