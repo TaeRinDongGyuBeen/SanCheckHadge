d8589ff9e686fe450997a6bc90a9ca2321167822
Taerogrammer <hyung54321@naver.com>
2023-06-04 23:21:48 +0900

    [Feat]SST#18 - 모달 높이에 따른 opacity 설정 및 기본 높이 수정

--- InToTheSeoul/InToTheSeoul/View/TrackingView/TrackingModalView.swift
+++ InToTheSeoul/InToTheSeoul/View/TrackingView/TrackingModalView.swift
@@ -9,19 +9,22 @@ import SwiftUI
 
 struct TrackingModalView: View {
     
-    @State var height: CGFloat = 100
-    let minHeight: CGFloat = 100
+    @State var height: CGFloat = 60
+    let minHeight: CGFloat = 60
     let maxHeight: CGFloat = 400
+    var percentage: Double {
+        Double(height / maxHeight)
+    }
     
     var body: some View {
-        
+
         VStack(spacing: 0) {
             
             //Handle
             ZStack {
                 Capsule()
                     .foregroundColor(Color.theme.gray3)
-                    .frame(width: 180, height: 10)
+                    .frame(width: 120, height: 10)
             }
             .frame(height: 40)
             .frame(maxWidth: .infinity)
@@ -29,48 +32,52 @@ struct TrackingModalView: View {
             
             VStack {
                 Text("Hello World")
-
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(.white)
             )
+            .padding(.all, 20)
+            .opacity(1.5 * (percentage - 0.3))
         }
         .frame(maxWidth: .infinity)
         .frame(height: height, alignment: .top)
         .background(Color.white)
         .clipShape(RoundedRectangle(cornerRadius: 20))
-        
     }
     
     var dragGesture: some Gesture {
         DragGesture(minimumDistance: 0)
-            .onChanged { val in
-                
-                var newHeight = height - val.translation.height
-                
-                if newHeight > maxHeight {
-                    newHeight = maxHeight
-                }
-                else if newHeight < minHeight {
-                    newHeight = minHeight
-                }
-                
-                height = newHeight
-                
-                print("Drage : \(height)")
-                
+        .onChanged { val in
+            
+            var newHeight = height - val.translation.height
+            
+            if newHeight > maxHeight {
+                newHeight = maxHeight
             }
-            .onEnded { val in
-                let percentage = height / maxHeight
-                var finalHeight = maxHeight
-                
-                if percentage < 0.45 {
-                    finalHeight = minHeight
-                }
-                withAnimation(Animation.easeOut(duration: 0.3)) {
-                    height = finalHeight
-                }
+            else if newHeight < minHeight {
+                newHeight = minHeight
             }
+            height = newHeight
+            
+        }
+        .onEnded { val in
+            let percentage = height / maxHeight
+            var finalHeight = maxHeight
+            
+            if percentage < 0.45 {
+                finalHeight = minHeight
+            }
+            withAnimation(Animation.easeOut(duration: 0.3)) {
+                height = finalHeight
+            }
+        }
+    }
+}
+
+
+struct TrackingModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackingModalView()
     }
 }
