810235eab40b93a37450e0e149badb48d5318be1
Taerogrammer <hyung54321@naver.com>
2023-06-05 09:54:02 +0900

    [Feat]SST#18-MKMapView 및 현재 위치 버튼 클릭 시 이동

--- InToTheSeoul/InToTheSeoul/View/TrackingView/View/MapView.swift
+++ InToTheSeoul/InToTheSeoul/View/TrackingView/View/MapView.swift
@@ -9,14 +9,191 @@ import CoreLocation
 import MapKit
 import SwiftUI
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+    
+    @Binding var showUserLocation: Bool
+    
+    @Binding var userLocation: CLLocationCoordinate2D?
+    
+    @Binding var region: MKCoordinateRegion
+    
+    @Binding var span: MKCoordinateSpan
+    
+    @ObservedObject var pointModel: PointsModel
+    
+    //UIViewRepresentable이 만들 View에 대한 정의를 해줘야 함
+    typealias UIViewType = MKMapView
+
+    //MapViewCoordinator가 coordinator임을 알려줘야 함
+    func makeCoordinator() -> MapViewCoordinator {
+        return MapViewCoordinator(userLocation: $userLocation, region: $region, span: $span)
+    }
+    
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        
+        //MapViewCoordinator에게 delegate 위임
+        mapView.delegate = context.coordinator
+        
+        //MARK: - 현재 위치 표시
+        mapView.showsUserLocation = showUserLocation
+
+        //유저 위치 설정
+        mapView.showsUserLocation = true
+        
+        
+        
+        //MARK: - region 설정
+
+        //region을 기준으로 map setting
+        mapView.setRegion(region, animated: true)
+        
+        // MARK: - JSON을 통해 불러온 데이터 어노테이션 추가
+//
+//        for busStop in busStopModel.busStopList {
+//            let annotation = MKPointAnnotation()
+//            annotation.coordinate = busStop.locationCoordinate
+//            annotation.title = busStop.stop_nm
+//            mapView.addAnnotation(annotation)
+//        }
+        
+        
+        
+        //NYC
+        let p1 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 40.71, longitude: -74))
+        
+        //Boston
+        let p2 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 42.36, longitude: -71.05))
+        
+        let p3 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 43.36, longitude: -72))
+        
+        let p4 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 44.01, longitude: -71.25))
+        let p5 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 41.12, longitude: -73.3))
+
+        //MARK: - 여러 경로
+        let placemarks: [MKPlacemark] = [p1, p2, p3, p4, p5] // 지점들의 배열
+
+        var directions: [MKDirections] = []
+
+        for i in 0..<placemarks.count {
+            let request = MKDirections.Request()
+            
+            // 출발지와 목적지 설정
+            request.source = MKMapItem(placemark: placemarks[i])
+            request.destination = MKMapItem(placemark: placemarks[(i+1) % placemarks.count])
+            
+            // 경로 옵션 설정
+            request.requestsAlternateRoutes = true
+            request.transportType = .walking
+            
+            let directionsRequest = MKDirections(request: request)
+            directions.append(directionsRequest)
+        }
+
+        for direction in directions {
+            direction.calculate { response, error in
+                guard let route = response?.routes.first else { return }
+                mapView.addOverlay(route.polyline)
+            }
+        }
+
+        // p1, p2, p3에 어노테이션 찍기
+        mapView.addAnnotations(placemarks)
+
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.setRegion(region, animated: true)
+        
+    }
+    
+    //MARK: - Coordinator 역할 (Delegate)
+    
+    class MapViewCoordinator: NSObject, MKMapViewDelegate, CLLocationManagerDelegate {
+        
+        //MARK: - 위치에 대한 변수
+        
+        var locationManager: CLLocationManager?
+
+        @Binding var userLocation: CLLocationCoordinate2D?
+        
+        @Binding var region: MKCoordinateRegion
+        
+        @Binding var span: MKCoordinateSpan
+        
+        init(userLocation: Binding<CLLocationCoordinate2D?>, region: Binding<MKCoordinateRegion>, span: Binding<MKCoordinateSpan>) {
+            _userLocation = userLocation
+            _region = region
+            _span = span
+            super.init()
+            checkIfLocationServicesIsEnabled()
+        }
+        
+        func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+            region = mapView.region
+            span = mapView.region.span // Update the span binding when the region changes
+        }
+        
+        //MARK: - 현재 위치 관련
+        
+        func checkIfLocationServicesIsEnabled() {
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager = CLLocationManager()
+                locationManager!.delegate = self
+                locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+            } else {
+                print("Show an alert")
+            }
+        }
+                
+        func checkLocationAuthorization() {
+            guard let locationManager = locationManager else { return }
+            
+            switch locationManager.authorizationStatus {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                print("Your location is restricted, likely due to parental controls.")
+            case .denied:
+                print("You have denied this app location permission. Go to Settings to change it.")
+            case .authorizedAlways, .authorizedWhenInUse:
+                let currentCoordinate = locationManager.location?.coordinate ?? CLLocationCoordinate2D()
+                let span = MKCoordinateSpan(latitudeDelta: region.span.latitudeDelta, longitudeDelta: region.span.longitudeDelta)
+                region = MKCoordinateRegion(center: currentCoordinate, span: span)
+                            
+                print("PPPPPPPPP \(currentCoordinate)")
+                print("region --- \(region)")
+                
+            @unknown default:
+                break
+            }
+        }
+        
+        func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+            checkLocationAuthorization()
+        }
+        
+        //MARK: - region 업데이트
+        
+        
+        
+        //MARK: - 경로 관련
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .blue
+            renderer.lineWidth = 5
+            
+            return renderer
+        }
     }
 }
 
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
-    }
-}
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView()
+//    }
+//}
--- InToTheSeoul/InToTheSeoul/View/TrackingView/View/TrackingView.swift
+++ InToTheSeoul/InToTheSeoul/View/TrackingView/View/TrackingView.swift
@@ -11,19 +11,44 @@ import CoreLocation
 
 struct TrackingView: View {
     
+    @State private var showUserLocation = false
+    
+    @State private var userLocation: CLLocationCoordinate2D?
+    
+    @State private var region = MKCoordinateRegion(center: DefaultLocation.startingLocation, span: DefaultLocation.defaultSpan)
+
+    @State private var span = DefaultLocation.defaultSpan
+    
+    @StateObject var pointModel = PointsModel()
+    
+    @State private var isNearby = false
+    
+    @State var time = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         ZStack {
             VStack {
-                MapView()
+                MapView(showUserLocation: $showUserLocation, userLocation: $userLocation, region: $region, span: $span, pointModel: pointModel)
                 
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .ignoresSafeArea()
+            .onAppear {
+                startBackgroundTask()
+            }
+            .onReceive(self.time) { (_) in
+                checkIsNear()
+            }
             
             //MARK: - 모달 뷰
             VStack(alignment: .trailing) {
                 Button(action: {
-                    
+                    getCurrentLocation { location in
+                        if let current = location {
+                            userLocation = current
+                            region = MKCoordinateRegion(center: current, span: span)
+                        }
+                    }
                 }, label: {
                     ZStack {
                         Circle()
@@ -45,6 +70,74 @@ struct TrackingView: View {
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea(edges: .bottom)
+        }
+    }
+    
+    func getCurrentLocation(completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+        let locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            
+            if let location = locationManager.location {
+                completion(location.coordinate)
+            } else {
+                completion(nil)
+            }
+        } else {
+            completion(nil)
+        }
+    }
+
+    
+    func startBackgroundTask() {
+        var backgroundTask: UIBackgroundTaskIdentifier = .invalid
+        DispatchQueue.global(qos: .background).async {
+            let backgroundTask = UIApplication.shared.beginBackgroundTask(withName: "Background Task") {
+                // 백그라운드 작업이 종료되었을 때 처리할 코드
+                UIApplication.shared.endBackgroundTask(backgroundTask)
+            }
+            while true {
+                // 백그라운드 작업 수행
+                Thread.sleep(forTimeInterval: 1)
+            }
+        }
+    }
+    
+    func checkIsNear() {
+
+        getCurrentLocation { coordinate in
+            guard let currentCoordinate = coordinate else {
+                print("현재 위치를 가져올 수 없음")
+                return
+            }
+            
+            print("CURRENTCOORDINATE :\(currentCoordinate)")
+            
+//            let targetLocation = busStopModel.busStopList[0...3]
+//
+//            DispatchQueue.global(qos: .background).async {
+//                let currentLocation = CLLocation(latitude: currentCoordinate.latitude, longitude: currentCoordinate.longitude)
+//                let maxDistance: CLLocationDistance = 400 // 최대 허용 거리 (예: 500 미터)
+//
+//                let isNearby = targetLocation.contains { location in
+//                    let locationCoordinate = CLLocationCoordinate2D(latitude: location.locationCoordinate.latitude, longitude: location.locationCoordinate.longitude)
+//                    let locationLocation = CLLocation(latitude: locationCoordinate.latitude, longitude: locationCoordinate.longitude)
+//
+////                                    print("current location : \(currentLocation)")
+//
+//                    let distance = currentLocation.distance(from: locationLocation)
+//
+//                    print("Distance : \(distance)")
+//                    return distance <= maxDistance
+//                }
+//
+//                DispatchQueue.main.async {
+//                    self.isNearby = isNearby
+//                }
+//            }
         }
     }
 }
