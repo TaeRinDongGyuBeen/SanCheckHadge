e3febf97f78658e76c0dff7affc12e50f3ceb872
dhk767 <dhk767@gmail.com>
2023-06-05 09:48:53 +0900

    [ADD] TimelineView 파일 생성

---
+++ InToTheSeoul/InToTheSeoul/View/TimelineView/TimelineView.swift
@@ -0,0 +1,21 @@
+//
+//  TimelineView.swift
+//  InToTheSeoul
+//
+//  Created by 김동현 on 2023/06/05.
+//
+
+import SwiftUI
+
+struct TimelineView: View {
+    var body: some View {
+        Text("Hello World")
+            .textFontAndColor(.h7)
+    }
+}
+
+struct TimelineView_Previewer: PreviewProvider {
+    static var previews: some View {
+        TimelineView()
+    }
+}
