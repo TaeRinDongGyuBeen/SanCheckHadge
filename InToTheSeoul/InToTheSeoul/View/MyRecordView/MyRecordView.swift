f2740302b54632add52d03c644e5a142e1ad189f
dhk767 <dhk767@gmail.com>
2023-06-05 12:11:42 +0900

    [ADD] MyRecordView 파일 추가

---
+++ InToTheSeoul/InToTheSeoul/View/MyRecordView/MyRecordView.swift
@@ -0,0 +1,21 @@
+//
+//  MyRecordView.swift
+//  InToTheSeoul
+//
+//  Created by 김동현 on 2023/06/05.
+//
+
+import SwiftUI
+
+struct MyRecordView: View {
+    var body: some View {
+        Text("Hello World")
+            .textFontAndColor(.h7)
+    }
+}
+
+struct MyRecordView_Previewer: PreviewProvider {
+    static var previews: some View {
+        MyRecordView()
+    }
+}
