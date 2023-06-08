c3cd7a2b8d40aece5e4605dfa81c77819dc74d68
dhk767 <dhk767@gmail.com>
2023-06-08 14:15:06 +0900

    [ADD] 온보딩 gif 파일 추가

--- InToTheSeoul/InToTheSeoul/View/OnboardingView/OnboardingView.swift
+++ InToTheSeoul/InToTheSeoul/View/OnboardingView/OnboardingView.swift
@@ -10,28 +10,28 @@ import WebKit
 
 struct GifImage: UIViewRepresentable {
     private let name: String
-    
+
     init(_ name: String) {
         self.name = name
     }
-    
+
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
         let url = Bundle.main.url(forResource: name, withExtension: "gif")!
         let data = try! Data(contentsOf: url)
-        
+
         webView.scrollView.isScrollEnabled = false
-        
+
         webView.load(
             data,
             mimeType: "image/gif",
             characterEncodingName: "UTF-8",
             baseURL: url.deletingLastPathComponent()
         )
-        
+
         return webView
     }
-    
+
     func updateUIView(_ uiView: WKWebView, context: Context) {
         uiView.reload()
     }
