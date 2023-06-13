2f2ca132bcd229e616992256514759d9148f6ae2
dhk767 <dhk767@gmail.com>
2023-06-13 21:02:18 +0900

    [feat] App entryPoint에 AppDelegate Class 추가

--- InToTheSeoul/InToTheSeoul/InToTheSeoulApp.swift
+++ InToTheSeoul/InToTheSeoul/InToTheSeoulApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct InToTheSeoulApp: App {
+    // register app delegate for Firebae setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var pointsModel: PointsModel = PointsModel()
     
     var body: some Scene {
